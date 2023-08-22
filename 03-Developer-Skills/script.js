2f2c7e4612b5408b1a4a8eb2a0959a97af5b9058
Febrian <solihinfebrian0@gmail.com>
2023-08-23 01:32:11 +0700

    Berhasil menyelesaikan 03-Developer-Skills

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,72 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = '23';
+const calAge = birthYear => 2037 - birthYear;
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+calcTempAmplitude([3, 7, 4]);
+// max = 3
+// max = 7
+const amplitute = calcTempAmplitude(temperatures);
+console.log(amplitute);
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplituteNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplituteNew);
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celcius',
+    value: Number(prompt('Degrees celcius:')),
+  };
+  console.table(measurement);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
+
+const data1 = [17, 21, 23];
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str = str + `${arr[i]}C in ${i + 1} days...`;
+  }
+  console.log('...', str);
+};
+printForecast(data1);
